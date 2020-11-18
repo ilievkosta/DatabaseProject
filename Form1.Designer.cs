6c82719e0ca90f37261a17ab729a85570605c34a
ilievkosta <47809260+ilievkosta@users.noreply.github.com>
2020-11-18 20:21:11 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace DatabaseProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelJobTitle = new System.Windows.Forms.Label();
+            this.labelText = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxJobTitle = new System.Windows.Forms.TextBox();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.buttonToDatabase = new System.Windows.Forms.Button();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.titleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Save = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Delete = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.databaseGritBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseGritBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(34, 20);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Name";
+            // 
+            // labelJobTitle
+            // 
+            this.labelJobTitle.AutoSize = true;
+            this.labelJobTitle.Location = new System.Drawing.Point(35, 59);
+            this.labelJobTitle.Name = "labelJobTitle";
+            this.labelJobTitle.Size = new System.Drawing.Size(47, 13);
+            this.labelJobTitle.TabIndex = 1;
+            this.labelJobTitle.Text = "Job Title";
+            // 
+            // labelText
+            // 
+            this.labelText.AutoSize = true;
+            this.labelText.Location = new System.Drawing.Point(271, 23);
+            this.labelText.Name = "labelText";
+            this.labelText.Size = new System.Drawing.Size(28, 13);
+            this.labelText.TabIndex = 2;
+            this.labelText.Text = "Text";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(132, 20);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 3;
+            // 
+            // textBoxJobTitle
+            // 
+            this.textBoxJobTitle.Location = new System.Drawing.Point(132, 52);
+            this.textBoxJobTitle.Name = "textBoxJobTitle";
+            this.textBoxJobTitle.Size = new System.Drawing.Size(100, 20);
+            this.textBoxJobTitle.TabIndex = 4;
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(369, 20);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Horizontal;
+            this.richTextBox.Size = new System.Drawing.Size(237, 67);
+            this.richTextBox.TabIndex = 5;
+            this.richTextBox.Text = "";
+            // 
+            // buttonToDatabase
+            // 
+            this.buttonToDatabase.Location = new System.Drawing.Point(679, 29);
+            this.buttonToDatabase.Name = "buttonToDatabase";
+            this.buttonToDatabase.Size = new System.Drawing.Size(119, 30);
+            this.buttonToDatabase.TabIndex = 6;
+            this.buttonToDatabase.Text = "Save to Database";
+            this.buttonToDatabase.UseVisualStyleBackColor = true;
+            this.buttonToDatabase.Click += new System.EventHandler(this.buttonToDatabase_Click);
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.AutoSize = true;
+            this.labelDescription.Location = new System.Drawing.Point(35, 90);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(60, 13);
+            this.labelDescription.TabIndex = 7;
+            this.labelDescription.Text = "Description";
+            this.labelDescription.Click += new System.EventHandler(this.labelDescription_Click);
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Location = new System.Drawing.Point(132, 90);
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDescription.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.nameDataGridViewTextBoxColumn,
+            this.titleDataGridViewTextBoxColumn,
+            this.descriptionDataGridViewTextBoxColumn,
+            this.textDataGridViewTextBoxColumn,
+            this.Save,
+            this.Delete});
+            this.dataGridView1.DataSource = this.databaseGritBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(38, 161);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(760, 263);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Id
+            // 
+            this.Id.DataPropertyName = "Id";
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // titleDataGridViewTextBoxColumn
+            // 
+            this.titleDataGridViewTextBoxColumn.DataPropertyName = "Title";
+            this.titleDataGridViewTextBoxColumn.HeaderText = "Title";
+            this.titleDataGridViewTextBoxColumn.Name = "titleDataGridViewTextBoxColumn";
+            // 
+            // descriptionDataGridViewTextBoxColumn
+            // 
+            this.descriptionDataGridViewTextBoxColumn.DataPropertyName = "Description";
+            this.descriptionDataGridViewTextBoxColumn.HeaderText = "Description";
+            this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
+            // 
+            // textDataGridViewTextBoxColumn
+            // 
+            this.textDataGridViewTextBoxColumn.DataPropertyName = "Text";
+            this.textDataGridViewTextBoxColumn.HeaderText = "Text";
+            this.textDataGridViewTextBoxColumn.Name = "textDataGridViewTextBoxColumn";
+            // 
+            // Save
+            // 
+            this.Save.DataPropertyName = "Save";
+            this.Save.HeaderText = "Save";
+            this.Save.Name = "Save";
+            this.Save.Text = "Save";
+            this.Save.ToolTipText = "Save";
+            this.Save.UseColumnTextForButtonValue = true;
+            // 
+            // Delete
+            // 
+            this.Delete.DataPropertyName = "Delete";
+            this.Delete.HeaderText = "Delete";
+            this.Delete.Name = "Delete";
+            this.Delete.Text = "Delete";
+            this.Delete.UseColumnTextForButtonValue = true;
+            // 
+            // databaseGritBindingSource
+            // 
+            this.databaseGritBindingSource.DataSource = typeof(DatabaseProject.DatabaseGrit);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 514);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBoxDescription);
+            this.Controls.Add(this.labelDescription);
+            this.Controls.Add(this.buttonToDatabase);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.textBoxJobTitle);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.labelText);
+            this.Controls.Add(this.labelJobTitle);
+            this.Controls.Add(this.labelName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseGritBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelJobTitle;
+        private System.Windows.Forms.Label labelText;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxJobTitle;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Button buttonToDatabase;
+        private System.Windows.Forms.Label labelDescription;
+        private System.Windows.Forms.TextBox textBoxDescription;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource databaseGritBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn titleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn textDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewButtonColumn Save;
+        private System.Windows.Forms.DataGridViewButtonColumn Delete;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Windows.Forms;
+
+namespace DatabaseProject
+{
+    public partial class Form1 : Form
+    {
+        public void LoadData()
+        {
+            string cs = @"URI=file:C:\Users\Game\source\repos\DatabaseProject\Coordinates.db";
+
+            SQLiteConnection con = new SQLiteConnection(cs);
+            con.Open();
+            string stm = "SELECT Id,Name,Job,Description,Cord FROM Cord";
+
+            SQLiteCommand cmd = new SQLiteCommand(stm, con);
+            SQLiteDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                databaseGritBindingSource.Add(new DatabaseGrit() {Id=rdr.GetInt32(0), Name = rdr.GetString(1), Title = rdr.GetString(2), Description = "Danni", Text = rdr.GetString(3) });
+
+            }
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            LoadData();
+            
+        }
+
+                public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void buttonToDatabase_Click(object sender, EventArgs e)
+        {
+            string cs = @"URI=file:C:\Users\Game\source\repos\DatabaseProject\Coordinates.db";
+
+            SQLiteConnection con = new SQLiteConnection(cs);
+            con.Open();
+
+            SQLiteCommand cmd = new SQLiteCommand(con);
+            string Name = textBoxName.Text;
+            string Job = textBoxJobTitle.Text;
+            string Cord = richTextBox.Text;
+            string Description = textBoxDescription.Text;
+
+
+            cmd.CommandText = "INSERT INTO Cord(Name, Job, Cord, Description) VALUES(@Name,@Job,@Cord,@Description)";
+            cmd.Parameters.AddWithValue("@Name", Name);
+            cmd.Parameters.AddWithValue("@Job", Job);
+            cmd.Parameters.AddWithValue("@Cord", Cord);
+            cmd.Parameters.AddWithValue("@Description", Description);
+            cmd.Prepare();
+
+            cmd.ExecuteNonQuery();
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+            LoadData();
+        }
+
+        
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelDescription_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.Columns[e.ColumnIndex].Name == "Delete" & dataGridView1.Rows.Count > 1)
+            {
+                if (MessageBox.Show("Are you sure", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    
+
+                    if (dataGridView1.CurrentRow != null)
+                    {
+                        string cs = @"URI=file:C:\Users\Game\source\repos\DatabaseProject\Coordinates.db";
+
+                        SQLiteConnection con = new SQLiteConnection(cs);
+                        con.Open();
+
+                        SQLiteCommand cmd = new SQLiteCommand(con);
+                        cmd.CommandText = "DELETE FROM Cord WHERE Id=@Id";
+                        int Id = (int)dataGridView1.CurrentRow.Cells[0].Value;
+                        cmd.Parameters.AddWithValue("@Id", Id);
+                        cmd.Prepare();
+
+                        cmd.ExecuteNonQuery();
+                    }
+                    databaseGritBindingSource.RemoveCurrent();
+
+                }
+            }
+        }
+    }
+}
